3f7080b308eada4a79877eb623ae69d91ea262b3
Logan Thomas <logannormanthomas@protonmail.com>
2021-05-05 21:46:50 +0000

    added more modularity to program with extra file

---
+++ input.js
@@ -0,0 +1,19 @@
+const net = require('net');
+
+const handleUserInput = () => {
+  if ('\u0003') {
+    process.exit();
+  }
+}
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+module.exports = {
+  setupInput
+};
--- play.js
+++ play.js
@@ -1,20 +1,9 @@
 const net = require("net");
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 // establishes a connection with the game server
-const handleUserInput = () => {
-  if ('\u0003') {
-    process.exit();
-  }
-}
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+
 setupInput();
 
 console.log("Connecting ...");
