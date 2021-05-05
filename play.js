8bf943b476a92416c513375b5b161d17d91b906c
Logan Thomas <logannormanthomas@protonmail.com>
2021-05-05 21:40:08 +0000

    added handleUserInput to play

--- play.js
+++ play.js
@@ -2,7 +2,20 @@ const net = require("net");
 const { connect } = require('./client');
 
 // establishes a connection with the game server
-
+const handleUserInput = () => {
+  if ('\u0003') {
+    process.exit();
+  }
+}
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+setupInput();
 
 console.log("Connecting ...");
 connect();
